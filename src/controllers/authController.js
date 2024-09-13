a92b6d8c2f5bc04b211b23319049c9ea09b6f922
HieuNghiaJp <nghialezsm@gmail.com>
2024-09-13 14:29:48 +0700

    update code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,72 +1,20 @@
-// const express = require('express');
-// const router = express.Router();
-// const userModel = require('../models/userModel');
-
-// // router.get('/login', (req, res) => {
-// //     res.render('login');
-// // });
-
-// router.get('/login', (req, res) => {
-//     if (req.session.user) {
-//         // Nếu người dùng đã đăng nhập, chuyển hướng về trang chủ
-//         return res.redirect('/');
-//     }
-//     res.render('login');
-// });
-
-// // Xử lý đăng nhập 
-// router.post('/login', (req, res) => {
-//     const username = req.body.username;
-// 	const password = req.body.password;
-    
-//     // Kiểm tra thông tin đăng nhập với cơ sở dữ liệu
-//     userModel.authenticateUser(username, password, (err, user) => {
-//         if (err) {
-//             return;
-//         }
-//         if (user) {
-//             req.session.user = username;
-//             res.redirect('/');
-//         } else {
-//             res.render('login');
-//         }
-//     });
-// });
-
-// // Trang chủ
-// router.get('/', (req, res) => {
-//     if (req.session.user) {
-//         res.render('index', { username: req.session.user });
-//     } else {
-//         res.redirect('/login');
-//     }
-// });
-
-// router.get('/logout', (req, res) => {
-//     req.session.destroy((err) => {
-//         if (err) {
-//             return res.status(400).send({ message: err });
-//         }
-//         res.redirect('/login');
-//     });
-// })
-
-// module.exports = router;
-
 const userModel = require('../models/userModel');
 
 exports.getLogin = (req, res) => {
-    res.render('login');
+    res.render('login', { error: null });
 };
 
 exports.postLogin = async (req, res) => {
     const { username, password } = req.body;
     try {
+        console.log('Attempting login for:', username);
         const user = await userModel.authenticateUser(username, password);
         if (user) {
             req.session.user = user;
+            console.log('Login successful for:', username);
             res.redirect('/');
         } else {
+            console.log('Login failed for:', username);
             res.render('login', { error: 'Tên đăng nhập hoặc mật khẩu không đúng' });
         }
     } catch (error) {
@@ -81,12 +29,12 @@ exports.logout = (req, res) => {
             console.error('Lỗi đăng xuất:', err);
             return res.status(500).render('error', { message: 'Không thể đăng xuất' });
         }
-        res.redirect('/login');
+        res.redirect('/auth/login');
     });
 };
 
 exports.getRegister = (req, res) => {
-    res.render('register');
+    res.render('auth/register');
 };
 
 exports.postRegister = async (req, res) => {
@@ -94,9 +42,9 @@ exports.postRegister = async (req, res) => {
     try {
         const newUser = await userModel.createUser(username, password, email);
         if (newUser) {
-            res.redirect('/login');
+            res.redirect('/auth/login');
         } else {
-            res.render('register', { error: 'Không thể đăng ký. Vui lòng thử lại.' });
+            res.render('auth/register', { error: 'Không thể đăng ký. Vui lòng thử lại.' });
         }
     } catch (error) {
         console.error('Lỗi đăng ký:', error);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Thêm route GET cho trang đăng nhập
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
-router.get('/logout', authController.logout);
-router.get('/register', authController.getRegister);
-router.post('/register', authController.postRegister);
 
 module.exports = router;
